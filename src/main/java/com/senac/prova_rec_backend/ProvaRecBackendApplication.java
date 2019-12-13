a3ba4ffb7f72fee6159c496271e61432b97b7e6e
Leandro da Silva Junior <leandro.sj.dev@gmail.com>
2019-12-13 12:38:03 -0300

    initial commit

---
+++ src/main/java/com/senac/prova_rec_backend/ProvaRecBackendApplication.java
@@ -0,0 +1,13 @@
+package com.senac.prova_rec_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaRecBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaRecBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senac/prova_rec_backend/ProvaRecBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.prova_rec_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaRecBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
